69553e8d63915bfada11697c0a3e3303cfcba22a
bvkxxrrxckz <aleksei.tselitsky@gmail.com>
2022-05-02 18:43:30 +0700

    Внесены мелкие изменения.

---
+++ NoteApp/NoteApp.UI/AboutForm.Designer.cs
@@ -0,0 +1,143 @@
+﻿
+namespace NoteApp.UI
+{
+    partial class AboutForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.NameLabel = new System.Windows.Forms.Label();
+            this.VersionLabel = new System.Windows.Forms.Label();
+            this.AuthorLabel = new System.Windows.Forms.Label();
+            this.EmailLinkLabel = new System.Windows.Forms.LinkLabel();
+            this.GitHubLinkLabel = new System.Windows.Forms.LinkLabel();
+            this.CopyrightsLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // NameLabel
+            // 
+            this.NameLabel.AutoSize = true;
+            this.NameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.NameLabel.Location = new System.Drawing.Point(11, 12);
+            this.NameLabel.Margin = new System.Windows.Forms.Padding(0, 0, 2, 2);
+            this.NameLabel.Name = "NameLabel";
+            this.NameLabel.Size = new System.Drawing.Size(115, 29);
+            this.NameLabel.TabIndex = 0;
+            this.NameLabel.Text = "NoteApp";
+            // 
+            // VersionLabel
+            // 
+            this.VersionLabel.AutoSize = true;
+            this.VersionLabel.Location = new System.Drawing.Point(13, 41);
+            this.VersionLabel.Margin = new System.Windows.Forms.Padding(0, 2, 2, 2);
+            this.VersionLabel.Name = "VersionLabel";
+            this.VersionLabel.Size = new System.Drawing.Size(45, 15);
+            this.VersionLabel.TabIndex = 1;
+            this.VersionLabel.Text = "v. 1.0.0";
+            // 
+            // AuthorLabel
+            // 
+            this.AuthorLabel.AutoSize = true;
+            this.AuthorLabel.Location = new System.Drawing.Point(11, 89);
+            this.AuthorLabel.Margin = new System.Windows.Forms.Padding(0, 2, 2, 2);
+            this.AuthorLabel.Name = "AuthorLabel";
+            this.AuthorLabel.Size = new System.Drawing.Size(138, 15);
+            this.AuthorLabel.TabIndex = 2;
+            this.AuthorLabel.Text = "Author: Tselitsky Aleksey";
+            // 
+            // EmailLinkLabel
+            // 
+            this.EmailLinkLabel.AutoSize = true;
+            this.EmailLinkLabel.LinkArea = new System.Windows.Forms.LinkArea(21, 43);
+            this.EmailLinkLabel.Location = new System.Drawing.Point(11, 131);
+            this.EmailLinkLabel.Margin = new System.Windows.Forms.Padding(0, 2, 2, 2);
+            this.EmailLinkLabel.Name = "EmailLinkLabel";
+            this.EmailLinkLabel.Size = new System.Drawing.Size(249, 17);
+            this.EmailLinkLabel.TabIndex = 3;
+            this.EmailLinkLabel.TabStop = true;
+            this.EmailLinkLabel.Text = "e-mail for feedback: aleksei.tselitsky@gmail.com";
+            this.EmailLinkLabel.UseCompatibleTextRendering = true;
+            this.EmailLinkLabel.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.EmailLinkLabel_LinkClicked);
+            // 
+            // GitHubLinkLabel
+            // 
+            this.GitHubLinkLabel.AutoSize = true;
+            this.GitHubLinkLabel.LinkArea = new System.Windows.Forms.LinkArea(8, 31);
+            this.GitHubLinkLabel.Location = new System.Drawing.Point(11, 152);
+            this.GitHubLinkLabel.Margin = new System.Windows.Forms.Padding(0, 2, 2, 2);
+            this.GitHubLinkLabel.Name = "GitHubLinkLabel";
+            this.GitHubLinkLabel.Size = new System.Drawing.Size(152, 17);
+            this.GitHubLinkLabel.TabIndex = 7;
+            this.GitHubLinkLabel.TabStop = true;
+            this.GitHubLinkLabel.Text = "GitHub: bvkxxrrxckz/NoteApp";
+            this.GitHubLinkLabel.UseCompatibleTextRendering = true;
+            this.GitHubLinkLabel.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.GitHubLinkLabel_LinkClicked);
+            // 
+            // CopyrightsLabel
+            // 
+            this.CopyrightsLabel.AutoSize = true;
+            this.CopyrightsLabel.Location = new System.Drawing.Point(11, 251);
+            this.CopyrightsLabel.Margin = new System.Windows.Forms.Padding(0, 2, 2, 0);
+            this.CopyrightsLabel.Name = "CopyrightsLabel";
+            this.CopyrightsLabel.Size = new System.Drawing.Size(140, 15);
+            this.CopyrightsLabel.TabIndex = 8;
+            this.CopyrightsLabel.Text = "2021 Tselitsky Aleksey ©";
+            // 
+            // AboutForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(378, 277);
+            this.Controls.Add(this.CopyrightsLabel);
+            this.Controls.Add(this.GitHubLinkLabel);
+            this.Controls.Add(this.EmailLinkLabel);
+            this.Controls.Add(this.AuthorLabel);
+            this.Controls.Add(this.VersionLabel);
+            this.Controls.Add(this.NameLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "AboutForm";
+            this.Padding = new System.Windows.Forms.Padding(11, 12, 11, 12);
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "About";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label NameLabel;
+        private System.Windows.Forms.Label VersionLabel;
+        private System.Windows.Forms.Label AuthorLabel;
+        private System.Windows.Forms.LinkLabel EmailLinkLabel;
+        private System.Windows.Forms.LinkLabel GitHubLinkLabel;
+        private System.Windows.Forms.Label CopyrightsLabel;
+    }
+}
